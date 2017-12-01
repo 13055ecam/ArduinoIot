1dac8409da3c925c5499fbc446cd808e29912f45
Alexandre Lenaerts <13055@ecam.be>
2017-12-01 01:41:10 +0100

    Project IOT code + sources

---
+++ ESP_HTTP_DH11.ino
@@ -0,0 +1,172 @@
+#include <Adafruit_Sensor.h>
+#include <ESP8266WiFi.h>
+#include "DHT.h"
+
+#define DHTTYPE DHT11   // DHT 11
+
+int led = 12;
+const char WiFiAPPSK[] = "sparkfun";
+
+
+// Web Server on port 80
+WiFiServer server(80);
+
+// DHT Sensor
+const int DHTPin = 5;
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);
+
+// Temporary variables
+static char celsiusTemp[7];
+static char fahrenheitTemp[7];
+static char humidityTemp[7];
+
+// only runs once on boot
+void setup()
+{
+  setupWiFi();
+  Serial.begin(115200);
+  delay(10);
+  pinMode(led, OUTPUT);
+  dht.begin();
+  
+  // Starting the web server
+  server.begin();
+  Serial.println("Web server running. Waiting for the ESP IP...");
+  delay(10000);
+  
+  // Printing the ESP IP address
+  Serial.println(WiFi.localIP());
+}
+
+// runs over and over again
+void loop() {
+    Serial.println(WiFi.localIP());
+  // Listenning for new clients
+  WiFiClient client = server.available();
+  
+  if (client) {
+    Serial.println("New client");
+    // bolean to locate when the http request ends
+    boolean blank_line = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+         //String req = client.readStringUntil('\r');
+         // Serial.println(req);
+            if (c == '\n' && blank_line) {
+            // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+            float h = dht.readHumidity();
+            // Read temperature as Celsius (the default)
+            float t = dht.readTemperature();
+            // Read temperature as Fahrenheit (isFahrenheit = true)
+            float f = dht.readTemperature(true);
+            // Check if any reads failed and exit early (to try again).
+            if (isnan(h) || isnan(t) || isnan(f)) {
+              Serial.println("Failed to read from DHT sensor!");
+              strcpy(celsiusTemp,"Failed");
+              strcpy(fahrenheitTemp, "Failed");
+              strcpy(humidityTemp, "Failed");         
+            }
+            else{
+              // Computes temperature values in Celsius + Fahrenheit and Humidity
+              float hic = dht.computeHeatIndex(t, h, false);       
+              dtostrf(hic, 6, 2, celsiusTemp);             
+              float hif = dht.computeHeatIndex(f, h);
+              dtostrf(hif, 6, 2, fahrenheitTemp);         
+              dtostrf(h, 6, 2, humidityTemp);
+              // You can delete the following Serial.print's, it's just for debugging purposes
+              Serial.print("Humidity: ");
+              Serial.print(h);
+              Serial.print(" %\t Temperature: ");
+              Serial.print(t);
+              Serial.print(" *C ");
+              Serial.print(f);
+              Serial.print(" *F\t Heat index: ");
+              Serial.print(hic);
+              Serial.print(" *C ");
+              Serial.print(hif);
+              Serial.print(" *F");
+              Serial.print("Humidity: ");
+              Serial.print(h);
+              Serial.print(" %\t Temperature: ");
+              Serial.print(t);
+              Serial.print(" *C ");
+              Serial.print(f);
+              Serial.print(" *F\t Heat index: ");
+              Serial.print(hic);
+              Serial.print(" *C ");
+              Serial.print(hif);
+              Serial.println(" *F");
+            }
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/html");
+            client.println("Connection: close");
+            client.println();
+            // your actual web page that displays temperature and humidity
+            client.println("<!DOCTYPE HTML>");
+            client.println("<html>");
+            client.println("<head></head><body><h1>ESP8266 - Temperature and Humidity</h1><h3>Temperature in Celsius: ");
+            client.println(celsiusTemp);
+            client.println("*C</h3><h3>Temperature in Fahrenheit: ");
+            client.println(fahrenheitTemp);
+            client.println("*F</h3><h3>Humidity: ");
+            client.println(humidityTemp);
+            client.println("%</h3><h3>");
+            //client.println("<form action='' method='get'>");
+            //client.println("<button name='' type='submit' value='led1'>Led on</button>");
+            //client.println("<button name='' type='submit' value='led0'>Led offn</button>");
+            
+//        if (req.indexOf("led1") != -1)
+//          {
+//               int val = 1;
+//               client.print("oui");
+//               digitalWrite(led,val);
+//          }
+//          if (req.indexOf("led0") != -1)
+//          {
+//               int val = 0;
+//               digitalWrite(led,val);
+//          }
+            client.println("</body></html>");     
+            break;
+        }
+        if (c == '\n') {
+          // when starts reading a new line
+          blank_line = true;
+        }
+        else if (c != '\r') {
+          // when finds a character on the current line
+          blank_line = false;
+        }
+      } 
+    }  
+    // closing the client connection
+    delay(1);
+    client.stop();
+    Serial.println("Client disconnected.");
+  }
+}
+void setupWiFi()
+{
+  WiFi.mode(WIFI_AP);
+
+  // Do a little work to get a unique-ish name. Append the
+  // last two bytes of the MAC (HEX'd) to "Thing-":
+  uint8_t mac[WL_MAC_ADDR_LENGTH];
+  WiFi.softAPmacAddress(mac);
+  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
+                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
+  macID.toUpperCase();
+  String AP_NameString = "ESP8266 Thing " + macID;
+
+  char AP_NameChar[AP_NameString.length() + 1];
+  memset(AP_NameChar, 0, AP_NameString.length() + 1);
+
+  for (int i=0; i<AP_NameString.length(); i++)
+    AP_NameChar[i] = AP_NameString.charAt(i);
+
+  WiFi.softAP(AP_NameChar, WiFiAPPSK);
+}
+
+
---
+++ ESP_Mqtt_DH11.ino
@@ -0,0 +1,177 @@
+/*
+  Projet d'apprentissage d'un objet connecté (IoT)  pour réaliser une sonde de température
+  ESP8266 + DHT22 + LED + MQTT + Home-Assistant
+  Projets DIY (https://www.projetsdiy.fr) - Mai 2016
+  Licence : MIT
+*/
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "DHT.h"          // Librairie des capteurs DHT
+
+#define wifi_ssid "WiFi-2.4-8353"
+#define wifi_password "ACBE04C1C6"
+
+const char WiFiAPPSK[] = "sparkfun";
+
+
+#define mqtt_server "192.168.1.44"
+#define mqtt_user "USERNAME"  //s'il a été configuré sur Mosquitto
+#define mqtt_password "PASSWORD" //idem
+
+#define temperature_topic "sensor/temperature"  //Topic température
+#define humidity_topic "sensor/humidity"        //Topic humidité
+
+//Buffer qui permet de décoder les messages MQTT reçus
+char message_buff[100];
+
+long lastMsg = 0;   //Horodatage du dernier message publié sur MQTT
+long lastRecu = 0;
+bool debug = false;  //Affiche sur la console si True
+
+#define DHTPIN D4    // Pin sur lequel est branché le DHT
+
+// Dé-commentez la ligne qui correspond à votre capteur 
+#define DHTTYPE DHT11       // DHT 11 
+//#define DHTTYPE DHT22         // DHT 22  (AM2302)
+
+//Création des objets
+DHT dht(DHTPIN, DHTTYPE);     
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+int led = 12;
+
+void setup() {
+  Serial.begin(9600);     //Facultatif pour le debug
+  pinMode(led,OUTPUT);     //Pin 2 
+  setup_wifi();           //On se connecte au réseau wifi
+  //setupWiFi();
+  client.setServer(mqtt_server, 1883);    //Configuration de la connexion au serveur MQTT
+  client.setCallback(callback);  //La fonction de callback qui est executée à chaque réception de message   
+  dht.begin();
+}
+
+//Connexion au réseau WiFi
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connexion a ");
+  Serial.println(wifi_ssid);
+
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Connexion WiFi etablie ");
+  Serial.print("=> Addresse IP : ");
+  Serial.print(WiFi.localIP());
+}
+
+//Reconnexion
+void reconnect() {
+  //Boucle jusqu'à obtenur une reconnexion
+  while (!client.connected()) {
+    Serial.print("Connexion au serveur MQTT...");
+    if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
+      Serial.println("OK");
+    } else {
+      Serial.print("KO, erreur : ");
+      Serial.print(client.state());
+      Serial.println(" On attend 5 secondes avant de recommencer");
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  //Envoi d'un message par minute
+  if (now - lastMsg > 1000 * 1) {
+    lastMsg = now;
+    //Lecture de l'humidité ambiante
+    float h = dht.readHumidity();
+    Serial.println(h);
+    // Lecture de la température en Celcius
+    float t = dht.readTemperature();
+    Serial.println(t);
+
+    //Inutile d'aller plus loin si le capteur ne renvoi rien
+    if ( isnan(t) || isnan(h)) {
+      Serial.println("Echec de lecture ! Verifiez votre capteur DHT");
+      return;
+    }
+  
+    if ( debug ) {
+      Serial.print("Temperature : ");
+      Serial.print(t);
+      Serial.print(" | Humidite : ");
+      Serial.println(h);
+    }  
+    client.publish(temperature_topic, String(t).c_str(), true);   //Publie la température sur le topic temperature_topic
+    client.publish(humidity_topic, String(h).c_str(), true);      //Et l'humidité
+  }
+  if (now - lastRecu > 100 ) {
+    lastRecu = now;
+    client.subscribe("homeassistant/switch1");
+  }
+}
+
+// Déclenche les actions à la réception d'un message
+// D'après http://m2mio.tumblr.com/post/30048662088/a-simple-example-arduino-mqtt-m2mio
+void callback(char* topic, byte* payload, unsigned int length) {
+
+  int i = 0;
+  if ( debug ) {
+    Serial.println("Message recu =>  topic: " + String(topic));
+    Serial.print(" | longueur: " + String(length,DEC));
+  }
+  // create character buffer with ending null terminator (string)
+  for(i=0; i<length; i++) {
+    message_buff[i] = payload[i];
+  }
+  message_buff[i] = '\0';
+  
+  String msgString = String(message_buff);
+  if ( debug ) {
+    Serial.println("Payload: " + msgString);
+  }
+  
+  if ( msgString == "ON" ) {
+    digitalWrite(led,HIGH);  
+  } else {
+    digitalWrite(led,LOW);  
+  }
+}
+
+void setupWiFi()
+{
+  WiFi.mode(WIFI_AP);
+
+  // Do a little work to get a unique-ish name. Append the
+  // last two bytes of the MAC (HEX'd) to "Thing-":
+  uint8_t mac[WL_MAC_ADDR_LENGTH];
+  WiFi.softAPmacAddress(mac);
+  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
+                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
+  macID.toUpperCase();
+  String AP_NameString = "ESP8266 Thing " + macID;
+
+  char AP_NameChar[AP_NameString.length() + 1];
+  memset(AP_NameChar, 0, AP_NameString.length() + 1);
+
+  for (int i=0; i<AP_NameString.length(); i++)
+    AP_NameChar[i] = AP_NameString.charAt(i);
+
+  WiFi.softAP(AP_NameChar, WiFiAPPSK);
+}
+
